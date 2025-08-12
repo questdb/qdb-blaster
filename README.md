56d0dda75226c4ba1899e92af59540045756927d
Adam Cimarosti <cimarosti@gmail.com>
2025-08-12 13:12:09 +0100

    updated config + readme notes

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # qdb-blaster
-Blast QuestDB with test data
+Blast QuestDB with test data.
+
+* Send data to multiple tables over ILP.
+* Each table has a pool of threads.
+* The data is randomly generated.
+
+## Set up your database
+For ingestion, in enterprise you need to set up appropriate users and permissions
+
+```sql
+CREATE USER test_user WITH PASSWORD 'pass';
+GRANT HTTP, PGWIRE TO test_user;
+
+GRANT CREATE TABLE TO test_user;
+GRANT ADD COLUMN, INSERT ON ALL TABLES TO test_user;
+GRANT SELECT ON ALL TABLES TO test_user;
+
+GRANT DROP TABLE ON ALL TABLES TO test_user;
+GRANT SELECT ON ALL TABLES TO test_user;
+GRANT HTTP TO test_user;
+GRANT CREATE TABLE TO test_user;
+GRANT ADD COLUMN, INSERT ON ALL TABLES TO test_user;
+
+-- for ILP/HTTP
+ALTER USER test_user CREATE TOKEN TYPE REST WITH TTL '3650d';
+
+-- for ILP/TCP
+CREATE SERVICE ACCOUNT test_ilp_user;
+GRANT CREATE TABLE TO test_ilp_user;
+GRANT ILP TO test_ilp_user;
+GRANT INSERT, ADD COLUMN ON ALL TABLES TO test_ilp_user;
+ALTER SERVICE ACCOUNT test_ilp_user CREATE TOKEN TYPE JWK;
+```
+
+## Configuring the blaster
+* Copy a config.
+* Alter the connection settings adding in any tokens and passwords as appropriate.
+* Generate the config with a script if too big (see `gen_big_toml.py`)
+
+## Running the blaster
+```
+cargo run --release path_to_config.toml
+```
--- gen_big_toml.py
+++ gen_big_toml.py
@@ -10,11 +10,11 @@ def generate_schema():
     return "[\n" + "\n".join(schema_lines) + "\n]"
 
 def generate_send_section():
-    return '''batch_pause = ["10ms", "100ms"]
-batch_size = [10000, 50000]
-parallel_senders = 4
-tot_rows = 25_000_000
-batches_connection_keepalive = 10
+    return '''batch_pause = ["50ms", "100ms"]
+batch_size = [5000, 10000]
+parallel_senders = 16
+tot_rows = 15_000_000
+batches_connection_keepalive = 20
 '''
 
 def main():
@@ -22,13 +22,19 @@ def main():
         # Write header
         f.write('debug = true\n\n')
         f.write('[database]\n')
-        f.write('ilp = "http::addr=localhost:9000;token=qt1cBkOuvc_8VFCMHRacRaDNp7DkzTtf9Cu1eh6rSuYfMM;"\n')
+        #f.write('ilp = "http::addr=localhost:9000;token=qt1cBkOuvc_8VFCMHRacRaDNp7DkzTtf9Cu1eh6rSuYfMM;"\n')
+        f.write(
+            'ilp = "tcp::addr=localhost:9009;protocol_version=2;'
+            'username=test_ilp_user;'
+            'token=NPo-BO1F2zr7LoWBEeBpY6ZfNQXRdhpyud3llmWUycg;'
+            'token_x=ayc3ehklyXSa4Pe1w-jVxqGZjh0jJMUECThWftnHnu0;'
+            'token_y=N0fRjqck8MrwcvLCuJK_lQexLaDV8ZSn-EKnNHsBX1o"\n')
         f.write('pgsql = "host=localhost port=8812 user=test_user password=pass dbname=qdb"\n\n')
 
         schema = generate_schema()
         send_section = generate_send_section()
 
-        for t in range(1, 21):
+        for t in range(1, 11):
             f.write(f'[tables.metrics{t}]\n')
             f.write(f'schema = {schema}\n')
             f.write('designated_ts = "timestamp"\n\n')
